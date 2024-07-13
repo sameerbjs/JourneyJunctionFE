ad84ba54e022b95fd9b7298d395c88356cdfcef4
Sameer <sabirsameer48@gmail.com>
2024-07-13 16:15:12 +0500

    Update provider.tsx

--- src/redux/provider.tsx
+++ src/redux/provider.tsx
@@ -5,8 +5,18 @@ import { persistor, store } from "./store";
 import { PersistGate } from "redux-persist/integration/react";
 import notification from "@/services/notification";
 import Header from "@/components/Header";
+import { useEffect } from "react";
+import { usePathname } from "next/navigation";
+import Api from "@/services/api";
 
 export function Providers({ children }: { children: React.ReactNode }) {
+
+    const pathName = usePathname();
+    const token = store.getState().auth.data?.token;
+
+    useEffect(()=>{
+       Api.setClientToken(token || '');
+    },[pathName])
     return (
         <PersistGate loading={null} persistor={persistor}>
             <Provider store={store}>
